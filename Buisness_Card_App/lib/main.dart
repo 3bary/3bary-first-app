a300d515e8c7924e9144681ec2300fcb8e243373
Mohamed Ashraf <medoalex350@gmail.com>
2022-04-26 05:03:46 +0200

    new edit

--- Buisness_Card_App/lib/main.dart
+++ Buisness_Card_App/lib/main.dart
@@ -12,9 +12,7 @@ class buisnessCardApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           backgroundColor: Colors.deepPurple,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+          body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             const CircleAvatar(
               radius: 103,
               backgroundColor: Colors.white,
@@ -28,12 +26,9 @@ class buisnessCardApp extends StatelessWidget {
               style: TextStyle(
                   color: Colors.white, fontSize: 30, fontFamily: 'Pacifico'),
             ),
-            Text(
-              'FLUTTER DEVELOPER',
-             style: TextStyle(
-               color: Colors.white.withOpacity(0.7), fontSize: 20
-             )
-            ),
+            Text('FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.white.withOpacity(0.7), fontSize: 20)),
             Divider(
               color: Colors.white.withOpacity(0.5),
               thickness: 1,
@@ -41,66 +36,37 @@ class buisnessCardApp extends StatelessWidget {
               endIndent: 60,
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 16),
-              child: Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              margin: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 32,
+                  color: Colors.deepPurple,
                 ),
-                child: Row(
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      child: Icon(
-                          Icons.phone,
-                        size: 32,
-                        color: Colors.deepPurple,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      child: Text(
-                        '(+20) 01095869392',
-                        style: TextStyle(fontSize: 24,color: Colors.deepPurple),
-                      ),
-                    ),
-                  ],
+                title: Text(
+                  '(+20) 01095869392',
+                  style: TextStyle(fontSize: 24, color: Colors.deepPurple),
                 ),
               ),
             ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 16),
-                  child: Container(
-                    height: 60,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: Row(
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.only(left: 20),
-                          child: Icon(
-                            Icons.mail,
-                            size: 32,
-                            color: Colors.deepPurple,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 20),
-                          child: Text(
-                            'medoalex350@gmail.com',
-                            style: TextStyle(fontSize: 20,color: Colors.deepPurple),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-          ])
-      ),
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              margin: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  size: 32,
+                  color: Colors.deepPurple,
+                ),
+                title: Text(
+                  'medoalex350@gmail.com',
+                  style: TextStyle(fontSize: 24, color: Colors.deepPurple),
+                ),
+              ),
+            ),
+          ])),
     );
   }
 }
